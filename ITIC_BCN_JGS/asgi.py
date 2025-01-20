551f180e9e2eb05b1d942b20605c8f17db2119d4
JavierGimSan <javi.gimsa@gmail.com>
2025-01-20 19:07:00 +0100

    Creació del projecte, creació de l'aplicació 'centre', migracions fetes i modificació de l'arxiu 'settings.py'

---
+++ ITIC_BCN_JGS/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ITIC_BCN_JGS project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ITIC_BCN_JGS.settings')
+
+application = get_asgi_application()
---
+++ ITIC_BCN_JGS/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ITIC_BCN_JGS project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ITIC_BCN_JGS.settings')
+
+application = get_wsgi_application()
