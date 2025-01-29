e6b09ebd4708ff5623f393088a9a3cfd3d6c4942
JavierGimSan <javi.gimsa@gmail.com>
2025-01-29 20:51:29 +0100

    Exercici 1 completat

--- centre/views.py
+++ centre/views.py
@@ -11,7 +11,8 @@ def index(request):
 
 #Funció per a l'exercici 1, endpoint "/teachers"
 def teachers(request):
-    professors = [{"name":"Roger", "surname1":"Sobrino", "surname2":"Sobrino", "email":"roger.sobrino@iticbcn.cat", "curs": "2", "tutor": "si", "modules": "M07"},
+    professors = [
+                 {"name":"Roger", "surname1":"Sobrino", "surname2":"Sobrino", "email":"roger.sobrino@iticbcn.cat", "curs": "2", "tutor": "si", "modules": "M07"},
                  {"name":"Oriol", "surname1":"Roca", "surname2":"Fabra", "email":"oriol.roca@iticbcn.cat", "curs": "2", "tutor": "no", "modules": "M06"},
                  {"name":"Juan Manuel", "surname1":"Sánchez", "surname2":"Bel", "email":"juan.sanchez@iticbcn.cat", "curs": "2", "tutor": "no", "modules": "M08"}
                  ]
@@ -19,11 +20,17 @@ def teachers(request):
     return render(request, 'teachers.html', context)
 
 def students(request):
-    alumnes = [{"name":"Victor Andrés", "surname1":"Fernández", "surname2":"Álvarez", "email":"2023_victor.fernandez@iticbcn.cat", "curs": "2", "modules": "M06, M07, M08, IA"},
+    alumnes = [
+               {"name":"Victor Andrés", "surname1":"Fernández", "surname2":"Álvarez", "email":"2023_victor.fernandez@iticbcn.cat", "curs": "2", "modules": "M06, M07, M08, IA"},
                {"name":"Xavier", "surname1":"Porras", "surname2":"del Pino", "email":"2023_xavier.porras@iticbcn.cat", "curs": "2", "modules": "M06, M07, M08"},
                {"name":"Hugo", "surname1":"Sansegundo", "surname2":"Costantini", "email":"2023_hugo.sansegundo@iticbcn.cat", "curs": "2", "modules": "M06, M07, M08, Big Data"},
-               {"name":" ", "surname1":" ", "surname2":" ", "email":" ", "curs": "2", "modules": " "}
-
+               {"name":"Adrián", "surname1":"Navarro", "surname2":"Pérez", "email":"2023_adrian.navarro@iticbcn.cat", "curs": "2", "modules": "M06, M07, M08"},
+               {"name":"Javier", "surname1":"Giménez", "surname2":"Sánchez", "email":"2023_javier.gimenez@iticbcn.cat", "curs": "2", "modules": "M06, M07, M08"},
+               {"name":"Daniel", "surname1":"Vallespin", "surname2":"Mellado", "email":"2023_daniel.vallespin@iticbcn.cat", "curs": "2", "modules": "M06, M07, M08"},
+               {"name":"Milena", "surname1":"Vardumyan", "surname2":"Aleksanyan", "email":"2023_milena.vardumyan@iticbcn.cat", "curs": "2", "modules": "M06, M07, M08"},
+               {"name":"Félix Bequet", "surname1":"Balbin", "surname2":"Silva", "email":"2023_felix.balbin@iticbcn.cat", "curs": "2", "modules": "M06, M07, M08"},
+               {"name":"Albert", "surname1":"Penadés", "surname2":"Casajús", "email":"2023_albert.penades@iticbcn.cat", "curs": "2", "modules": "M06, M07, M08"},
+               {"name":"Natalia", "surname1":"Casanellas", "surname2":"Blanquer", "email":"2023_natalia.casanellas@iticbcn.cat", "curs": "2", "modules": "M06, M07, M08"},   
                ]
     context = {'alumnes': alumnes}
     return render(request, 'students.html', context)
