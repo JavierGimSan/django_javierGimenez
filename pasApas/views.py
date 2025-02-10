8b72b42d8339d9d96311bcf329607248911021dd
JavierGimSan <javi.gimsa@gmail.com>
2025-02-10 17:07:37 +0100

    Posada al día, modificació de views que no vaig guardar la sessió anterior

--- pasApas/views.py
+++ pasApas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 def guardar_sesion(request):
     request.session['usuario'] = 'Juan'
@@ -8,5 +8,8 @@ def recuperar_sesion(request):
     usuario = request.session.get('usuario', 'Invitado')
     return render(request, 'recuperar_sesion.html', {'usuario': usuario})
 
-
+def eliminar_sesion(request):
+    if 'usuario' in request.session:
+        del request.sesion['usuario']
+        return redirect('recuperar_sesion')
 
